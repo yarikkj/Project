2d7666fa2d85107ca4040454e0ea1df939d91f23
yarikkj <yarikkj99@gmai.com>
2019-03-23 16:09:13 +0200

    first commit

---
+++ Node js First/server.js
@@ -0,0 +1,20 @@
+// берём Express
+var express = require('express');
+
+// создаём Express-приложение
+var app = express();
+
+// создаём маршрут для главной страницы
+// http://localhost:8080/
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+
+app.get('/hi', function(req, res) {
+  res.sendfile('hi.html');
+});
+
+// запускаем сервер на порту 8080
+app.listen(8080);
+// отправляем сообщение
+console.log('Сервер стартовал!');
